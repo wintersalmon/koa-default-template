7c6e58d2ef16bdb1d69ad206dd620de97c811f10
Hungjoon Kim <wintersalmon@gmail.com>
2020-01-11 16:41:26 +0900

    Improve debug env

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
+/* eslint-disable no-console */
+import dotenv from 'dotenv';
 import Koa from 'koa';
+import chalk from 'chalk';
 
+dotenv.config();
 const app = new Koa();
 
 app.use(async ctx => {
@@ -7,3 +11,12 @@ app.use(async ctx => {
 });
 
 app.listen(3000);
+
+app.listen(process.env.PORT, () => {
+  const { red, yellow } = chalk.bold;
+  console.log(yellow('######################################'));
+  console.log(yellow(`NODE_ENV            : ${red(process.env.NODE_ENV || '-')}`));
+  console.log(yellow(`NODE_CONFIG_ENV     : ${red(process.env.NODE_CONFIG_ENV || '-')}`));
+  console.log(yellow(`SERVER_PORT         : ${red(process.env.PORT || '-')}`));
+  console.log(yellow('######################################'));
+});
